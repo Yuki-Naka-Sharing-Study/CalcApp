f1aa3c30c0cc8ba79525729a857fac64807103fc
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2020-12-19 20:59:38 +0900

    1つ目の画面では、四則計算用のそれぞれのボタンを4つ作成。 ただし、それぞれのボタンはまだ四則計算できない。

--- app/src/main/java/jp/techacademy/yuki/naka/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/calcapp/MainActivity.kt
@@ -2,10 +2,16 @@ package jp.techacademy.yuki.naka.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            // EditTextの文字列をTextViewに設定
+            textView.text = editText.text.toString()
+        }
     }
 }
