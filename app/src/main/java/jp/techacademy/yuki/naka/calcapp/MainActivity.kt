b9234c6cbe94379d009686195690c4c38aa43795
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2020-12-20 17:21:25 +0900

    『RelativeLayout』から『LinearLayout』に変更。 （activity_main.xml）

--- app/src/main/java/jp/techacademy/yuki/naka/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/calcapp/MainActivity.kt
@@ -2,9 +2,11 @@ package jp.techacademy.yuki.naka.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             // EditTextの文字列をTextViewに設定
             textView.text = editText2.text.toString()
         }
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", 10)
+        intent.putExtra("VALUE2", 20)
+        startActivity(intent)
     }
 }
